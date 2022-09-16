95812fad61752844b442c8ab70c7496f67721898
Thibaut STRECKER <thibaut.strecker@gmail.com>
2022-09-16 15:06:00 +0200

    feat: initial commit

---
+++ src/main/java/org/example/FizzBuzz.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/*
+    FizzBuzz
+
+You need to write a program that prints the numbers from 1 to n such that:
+
+  * If the number is a multiple of 3, you need to print "Fizz" instead of that number.
+  * If the number is a multiple of 5, you need to print "Buzz" instead of that number.
+  * If the number is a multiple of both 3 and 5, you need to print "FizzBuzz" instead of that number.
+
+ */
+
+public class FizzBuzz {
+
+}
